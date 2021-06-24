e3b7e436e3bbc19d7bb24de8290df6b3293635c4
Willem Meints <1550763+wmeints@users.noreply.github.com>
2021-06-24 16:19:41 +0200

    Complete setup for client skeleton

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,14 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.tsx'),
+    entry: {
+      index: {
+          import: path.resolve(__dirname, 'src/index.tsx'),
+          dependOn: 'shared'
+      },
+      shared: 'react'
+    },
     output: {
-        filename: 'app.[chunkhash].js',
-        path: path.resolve(__dirname, 'dist')
+        filename: 'app.[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx']
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss', '.css']
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            chunks: 'all'
+        }
     },
     module: {
         rules: [
@@ -23,13 +38,38 @@ module.exports = {
                         '@babel/preset-typescript'
                     ],
                 }
+            },
+            {
+                test: /scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('node-sass')
+                        }
+                    }
+                ]
             }
         ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 3000
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public/index.html'),
             compress: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'app.[name].[chunkhash].css'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public/images', to: 'dist/images' }
+            ]
         })
     ]
 }
