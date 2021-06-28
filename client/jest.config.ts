ad086220421644a1c0f0060bfb34ad7d5f7160b3
Willem Meints <1550763+wmeints@users.noreply.github.com>
2021-06-28 11:10:54 +0200

    Update root folder

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -117,7 +117,7 @@ export default {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: 'src',
+  rootDir: '.',
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
